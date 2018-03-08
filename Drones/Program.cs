754ea9229cfa3fb08219aaf8868a2e83c3df6a8c
Stephen JANNIN <stephen.jannin@sgcib.com>
2018-03-08 21:56:19 +0100

    Class containing Disposable resources should implement IDisposable

--- Drones/Program.cs
+++ Drones/Program.cs
@@ -15,32 +15,34 @@ namespace Drones
                 filename = args[1];
 
             BattlefieldBuilder battleFieldBuilder = null;
-            var commandFileReader = new CommandFileReader(filename);
-            var remoteController = new RemoteControl();
-            Drone drone = null;
-            foreach (var command in commandFileReader.Readline())
+            using(var commandFileReader = new CommandFileReader(filename))
             {
-                switch(command.CommandType)
+                var remoteController = new RemoteControl();
+                Drone drone = null;
+                foreach (var command in commandFileReader.Readline())
                 {
-                    case CommandLineType.UpperRightCoordinates:
-                        battleFieldBuilder = new BattlefieldBuilder(command.Position);
-                        break;
-                    case CommandLineType.DroneInit:
-                        drone = battleFieldBuilder.GetDrone(command.Position, command.Direction);
-                        Console.WriteLine("Drone Initial position: " + drone.ToString());
-                        break;
-                    case CommandLineType.Moves:
-                        if (drone == null)
+                    switch (command.CommandType)
+                    {
+                        case CommandLineType.UpperRightCoordinates:
+                            battleFieldBuilder = new BattlefieldBuilder(command.Position);
                             break;
-                        foreach (var c in command.DroneMoves)
-                            RemoteControl.SendCommandToDrone(c, drone);
-                        Console.WriteLine("Moves :" + command.Text);
-                        Console.WriteLine("Drone Final position: " + drone.ToString());
-                        break;
+                        case CommandLineType.DroneInit:
+                            drone = battleFieldBuilder.GetDrone(command.Position, command.Direction);
+                            Console.WriteLine("Drone Initial position: " + drone.ToString());
+                            break;
+                        case CommandLineType.Moves:
+                            if (drone == null)
+                                break;
+                            foreach (var c in command.DroneMoves)
+                                RemoteControl.SendCommandToDrone(c, drone);
+                            Console.WriteLine("Moves :" + command.Text);
+                            Console.WriteLine("Drone Final position: " + drone.ToString());
+                            break;
 
-                    case CommandLineType.Unknown:
-                        Console.WriteLine("Unknown command");
-                        break;
+                        case CommandLineType.Unknown:
+                            Console.WriteLine("Unknown command");
+                            break;
+                    }
                 }
             }
             Console.ReadKey();
